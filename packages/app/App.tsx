f472b293d04d7f387ebcd190f1271d37d4c41a5c
Mitchell Fraser <makerfrazer@gmail.com>
2021-05-24 12:08:23 -0700

    Add basic web wallet connect (requires nodeJS injection to work on native)

--- packages/app/App.tsx
+++ packages/app/App.tsx
@@ -1,11 +1,47 @@
 import 'react-native-gesture-handler';
 
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {
+  useWalletConnect,
+  withWalletConnect,
+} from '@walletconnect/react-native-dapp';
+import { StatusBar } from 'expo-status-bar';
+
 import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, Button, View, Platform } from 'react-native';
 
 import { linking } from './navigation/linking';
 import { RootNavigator } from './navigation/RootNavigator';
+
+
+const WalletConnectUI: React.FC = () => {
+  const connector = useWalletConnect();
+  if (!connector.connected) {
+    // Connect wallet before continuing
+    return (
+      <Button
+        title="Connect"
+        onPress={() => {
+          connector.connect();
+        }}
+      />
+    );
+  }
+
+  // User is connected
+  return (
+    <View style={styles.container}>
+      <Button
+        title="Kill session"
+        onPress={() => {
+          connector.killSession();
+        }}
+      />
+      <StatusBar style="auto" />
+    </View>
+  );
+};
 
 const App: React.FC = () => (
   <NavigationContainer linking={linking} fallback={<Text>Loading...</Text>}>
@@ -14,4 +50,9 @@ const App: React.FC = () => (
 );
 
 // eslint-disable-next-line import/no-default-export
-export default App;
+export default withWalletConnect(App, {
+  redirectUrl: Platform.OS === 'web' ? window.location.origin : 'appScheme://',
+  storageOptions: {
+    asyncStorage: AsyncStorage,
+  },
+});
