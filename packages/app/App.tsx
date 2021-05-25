49bf40bf678a024d6ef26fce311c8ecd48bd35a7
Mitchell Fraser <makerfrazer@gmail.com>
2021-05-24 17:23:40 -0700

    ignore typecheck on asyncstorage

--- packages/app/App.tsx
+++ packages/app/App.tsx
@@ -54,6 +54,8 @@ const App: React.FC = () => (
 export default withWalletConnect(App, {
   redirectUrl: Platform.OS === 'web' ? window.location.origin : 'appScheme://',
   storageOptions: {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
     asyncStorage: AsyncStorage,
   },
 });
