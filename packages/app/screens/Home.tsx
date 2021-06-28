460d1ff34bf9d0c31061a5fa36ef8a245cfe09e5
Hammad Jutt <jutt@ualberta.ca>
2021-06-28 10:11:14 -0600

    Setup route based code splitting for expo PWA

--- packages/app/screens/Home.tsx
+++ packages/app/screens/Home.tsx
@@ -1,32 +1,27 @@
-import { BaseComponent } from '@mf/components';
+import { PageContainer } from '@mf/components';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text } from 'react-native';
 
 import { MainStackScreenProps, Screen } from '../navigation/types';
 
 type Props = MainStackScreenProps<Screen.HOME>;
 
 export const HomeScreen: React.FC<Props> = ({ navigation }) => (
-  <View style={styles.container}>
+  <PageContainer>
     <Text style={styles.text}>Home</Text>
-    <BaseComponent />
     <Button
       title="Go To Screen"
       onPress={() => navigation.navigate(Screen.PROPOSAL, { proposalId: '1' })}
     />
     <StatusBar style="auto" />
-  </View>
+  </PageContainer>
 );
 
+// eslint-disable-next-line import/no-default-export
+export default HomeScreen;
+
 const styles = StyleSheet.create({
-  // eslint-disable-next-line react-native/no-color-literals
-  container: {
-    alignItems: 'center',
-    backgroundColor: 'white',
-    flex: 1,
-    justifyContent: 'center',
-  },
   text: {
     fontSize: 24,
     margin: 10,
--- packages/app/screens/Proposal.tsx
+++ packages/app/screens/Proposal.tsx
@@ -1,24 +1,21 @@
+import { PageContainer } from '@mf/components/PageContainer';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text } from 'react-native';
 
 import { MainStackScreenProps, Screen } from '../navigation/types';
 
 type Props = MainStackScreenProps<Screen.PROPOSAL>;
 
 export const ProposalScreen: React.FC<Props> = () => (
-  <View style={styles.container}>
+  <PageContainer>
     <Text style={styles.text}>Proposal</Text>
-  </View>
+  </PageContainer>
 );
 
+// eslint-disable-next-line import/no-default-export
+export default ProposalScreen;
+
 const styles = StyleSheet.create({
-  // eslint-disable-next-line react-native/no-color-literals
-  container: {
-    alignItems: 'center',
-    backgroundColor: 'white',
-    flex: 1,
-    justifyContent: 'center',
-  },
   text: {
     fontSize: 24,
     margin: 10,
