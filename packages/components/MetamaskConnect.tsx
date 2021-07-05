a4b5d3217e7022d5038ef6f49d302e404fa398e2
Mitchell Fraser <makerfrazer@gmail.com>
2021-07-05 10:46:59 -0700

    remove unneeded dependency (ts enforces React.useState)

--- packages/components/MetamaskConnect.tsx
+++ packages/components/MetamaskConnect.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { Button, Platform, Text, View } from 'react-native';
 
 import { connectWallet, getCurrentWalletConnected } from './util/mfWeb3';
