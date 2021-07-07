3360dfa9767e1477b55c81706f2bc7dacce71108
Hammad Jutt <jutt@ualberta.ca>
2021-07-06 19:58:14 -0600

    Implement Button component in design system

--- packages/components/index.tsx
+++ packages/components/index.tsx
@@ -1,3 +1,4 @@
+export { Button } from './input/Button';
 export { Box } from './layout/Box';
 export { ScreenContainer } from './layout/ScreenContainer';
 export { StyledText } from './typography/StyledText';
