b87d7b8bb8d13af1c3027f80c1801ed5fb3ce65b
META DREAMER <jutt@ualberta.ca>
2022-06-05 14:36:51 -0600

    Set NFT tokenIds when generating metadata

---
+++ packages/services/lib/setNftTokenIds.ts
@@ -0,0 +1,31 @@
+import { order_by } from '../graphql/__generated__/zeus';
+import { client } from '../graphql/client';
+
+export async function setNftTokenIds(): Promise<void> {
+  const data = await client.query({
+    robot_product: [
+      {
+        where: { nft_metadata: { _is_null: false } },
+        order_by: [{ shopify_id: order_by.asc }],
+      },
+      {
+        id: true,
+        nft_token_id: true,
+        nft_metadata: [{}, true],
+      },
+    ],
+  });
+
+  for (let i = 0; i < data.robot_product.length; i += 1) {
+    // eslint-disable-next-line no-await-in-loop
+    await client.mutate({
+      update_robot_product_by_pk: [
+        {
+          pk_columns: { id: data.robot_product[i].id },
+          _set: { nft_token_id: i + 1 },
+        },
+        { nft_token_id: true },
+      ],
+    });
+  }
+}
