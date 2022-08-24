ca8a7d234f4ae842eea062feae4f4b3d221e91fa
META DREAMER <jutt@ualberta.ca>
2022-08-24 13:09:58 -0600

    Update directus to 9.16

---
+++ packages/cms/custom_migrations/20200824A-test-migration.js
@@ -0,0 +1,11 @@
+module.exports = {
+  up() {
+    // eslint-disable-next-line no-console
+    console.log('up');
+  },
+
+  down() {
+    // eslint-disable-next-line no-console
+    console.log('down');
+  },
+};
