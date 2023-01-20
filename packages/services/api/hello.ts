28c3d2ae3e29a059c6ecde4d29b3248fc7886608
META DREAMER <jutt@ualberta.ca>
2023-01-19 23:17:22 -0700

    Remove services/api since it was moved to apps/next

--- packages/services/api/hello.ts
+++
@@ -1,8 +0,0 @@
-import { VercelRequest, VercelResponse } from '@vercel/node';
-
-export default (req: VercelRequest, res: VercelResponse): void => {
-  const { name = 'World' } = req.query;
-
-  // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-  res.status(200).send(`Hello ${name}!`);
-};
