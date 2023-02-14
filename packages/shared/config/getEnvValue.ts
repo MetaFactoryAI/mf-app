8bf004bd3683331fa609a544cc15c914efebc365
Scott Herren <me@stwoh.com>
2023-02-14 14:51:33 -0600

    tweak env fetch for clarity

--- packages/shared/config/getEnvValue.ts
+++ packages/shared/config/getEnvValue.ts
@@ -2,12 +2,15 @@ export function getEnvValue<T extends string | number>(
   v: string | undefined,
   defaultVal?: T,
 ): T {
-  if (v) {
-    return typeof defaultVal === 'number' ? (Number(v) as T) : (v as T);
+  if (v && process.env[v]) {
+    return typeof defaultVal === 'number'
+      ? (Number(v) as T)
+      : (process.env[v] as T);
   }
 
-  if (defaultVal === undefined) throw new Error('Missing ENV Variable');
+  if (defaultVal === undefined)
+    throw new Error(`Missing ENV Variable: ${v} and NO DEFAULT AVAILABLE`);
 
-  console.warn(`Using default ENV variable: ${defaultVal}`);
+  console.warn(`Using default ENV variable: ${defaultVal} for value: ${v}`);
   return defaultVal;
 }
