882014b4c84542e7f6e264ab21dce07f8bd9c8d0
META DREAMER <jutt@ualberta.ca>
2023-10-22 19:31:05 -0400

    Update packages / Next

--- packages/app/ui/theme/index.js
+++ packages/app/ui/theme/index.js
@@ -1,26 +1,6 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 // @ts-check
-const { toRadixVar, toRadixVars } = require('windy-radix-palette/vars');
-
-/**
- * Generates CSS variables for given set of colors
- * @param {{ [s: string]: { [s: string]: any; } | string; }} colors */
-const createAliasColorVariables = (colors) => {
-  /** @type {{ [s: string]: any; }} */
-  let colorMap = {};
-
-  for (const [colorName, colorObj] of Object.entries(colors)) {
-    if (typeof colorObj === 'string') {
-      colorMap[`--${colorName}`] = colorObj;
-    } else if (colorObj) {
-      for (const [key, value] of Object.entries(colorObj)) {
-        colorMap[`--${colorName}${key}`] = value;
-      }
-    }
-  }
-
-  return colorMap;
-};
+const { alias } = require('windy-radix-palette');
 
 /** @param {{ [s: string]: any; }} aliases */
 const createAliasTheme = (aliases) => {
@@ -31,7 +11,7 @@ const createAliasTheme = (aliases) => {
     if (typeof colorObj === 'string') {
       themeColors[colorName] = `var(--${colorName})`;
     } else if (colorObj) {
-      themeColors[colorName] = toRadixVars(colorName);
+      themeColors[colorName] = alias(colorName);
     }
   }
 
@@ -39,35 +19,35 @@ const createAliasTheme = (aliases) => {
 };
 
 const aliasLightColors = {
-  appBg: toRadixVar('gray', 9),
-  appBgSubtle: toRadixVar('gray', 10),
-  panel: toRadixVar('gray', 3),
-  panelHover: toRadixVar('gray', 4),
-  shadow: toRadixVar('grayA', 3),
-  overlay: toRadixVar('blackA', 9),
-  danger: toRadixVar('red', 9),
-  success: toRadixVar('green', 9),
+  appBg: alias('gray', 9),
+  appBgSubtle: alias('gray', 10),
+  panel: alias('gray', 3),
+  panelHover: alias('gray', 4),
+  shadow: alias('grayA', 3),
+  overlay: alias('blackA', 9),
+  danger: alias('red', 9),
+  success: alias('green', 9),
 
-  grayBg: toRadixVar('gray', 9),
-  grayBgSubtle: toRadixVar('gray', 10),
-  grayBgHover: toRadixVar('gray', 9),
-  grayBgActive: toRadixVar('gray', 10),
-  grayText: toRadixVar('gray', 12),
-  grayDivider: toRadixVar('gray', 11),
-  grayTextSubtle: toRadixVar('gray', 11),
-  grayTranslucent: toRadixVar('grayA', 2),
+  grayBg: alias('gray', 9),
+  grayBgSubtle: alias('gray', 10),
+  grayBgHover: alias('gray', 9),
+  grayBgActive: alias('gray', 10),
+  grayText: alias('gray', 12),
+  grayDivider: alias('gray', 11),
+  grayTextSubtle: alias('gray', 11),
+  grayTranslucent: alias('grayA', 2),
 
-  brandText: toRadixVar('lime', 11),
-  brandTextContrast: toRadixVar('lime', 11),
-  brandBg: toRadixVar('lime', 9),
-  brandBgSubtle: toRadixVar('lime', 8),
-  brandBgHover: toRadixVar('lime', 9),
-  brandBgActive: toRadixVar('lime', 10),
+  brandText: alias('lime', 11),
+  brandTextContrast: alias('lime', 11),
+  brandBg: alias('lime', 9),
+  brandBgSubtle: alias('lime', 8),
+  brandBgHover: alias('lime', 9),
+  brandBgActive: alias('lime', 10),
 
-  borderLightest: toRadixVar('gray', 1),
-  borderLight: toRadixVar('whiteA', 11),
-  borderDark: toRadixVar('blackA', 9),
-  borderDarkest: toRadixVar('blackA', 12),
+  borderLightest: alias('gray', 1),
+  borderLight: alias('whiteA', 11),
+  borderDark: alias('blackA', 9),
+  borderDarkest: alias('blackA', 12),
 };
 const lightVars = {
   boxRetro:
@@ -105,11 +85,11 @@ const theme = {
     },
     colors: {
       ...aliasTheme,
-      brand: toRadixVars('lime'),
-      // appBg: toRadixVar('gray', 9),
-      // appBgSubtle: toRadixVar('gray', 10),
-      // panel: toRadixVar('gray', 3),
-      // panelHover: toRadixVar('gray', 4),
+      brand: alias('lime'),
+      // appBg: alias('gray', 9),
+      // appBgSubtle: alias('gray', 10),
+      // panel: alias('gray', 3),
+      // panelHover: alias('gray', 4),
     },
     spacing: {
       'nav-height': '72px',
