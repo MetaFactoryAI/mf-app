2bb2901b7d60b86448eb8a602d4f4b9435343a33
META DREAMER <jutt@ualberta.ca>
2024-07-12 17:15:05 -0600

    export in custom migrations

--- packages/cms/custom_migrations/20220825A-migrate-price-currencies.js
+++ packages/cms/custom_migrations/20220825A-migrate-price-currencies.js
@@ -1,31 +1,27 @@
-module.exports = {
-  async up() {
-    // add default USD, ETH for currencies
-    await knex('currencies').insert({ currency: 'USD' });
-    await knex('currencies').insert({ currency: 'ETH' });
+export async function up(knex) {
+  // add default USD, ETH for currencies
+  await knex('currencies').insert({ currency: 'USD' });
+  await knex('currencies').insert({ currency: 'ETH' });
 
-    const priceCurrencies = await knex('price_currencies');
-    const products = await knex('products').whereNotNull('price');
-    const currencies = await knex('currencies');
+  const priceCurrencies = await knex('price_currencies');
+  const products = await knex('products').whereNotNull('price');
+  const currencies = await knex('currencies');
 
-    for (let i = 0; i < products.length; i++) {
-      const product = products[i];
-      // get price currency for product
-      if (!product) continue;
-      const priceCurrency = priceCurrencies.find(
-        (pc) => pc.id === product.price,
-      );
-      // corresponding values to update
-      if (!priceCurrency) continue;
-      const sale_price = priceCurrency.amount;
-      const sale_currency = currencies.find(
-        (c) => c.currency === priceCurrency.currency,
-      );
-      if (sale_price && sale_currency) {
-        await knex('products')
-          .where({ id: product.id })
-          .update({ sale_price, sale_currency: sale_currency.id });
-      }
+  for (let i = 0; i < products.length; i++) {
+    const product = products[i];
+    // get price currency for product
+    if (!product) continue;
+    const priceCurrency = priceCurrencies.find((pc) => pc.id === product.price);
+    // corresponding values to update
+    if (!priceCurrency) continue;
+    const sale_price = priceCurrency.amount;
+    const sale_currency = currencies.find(
+      (c) => c.currency === priceCurrency.currency,
+    );
+    if (sale_price && sale_currency) {
+      await knex('products')
+        .where({ id: product.id })
+        .update({ sale_price, sale_currency: sale_currency.id });
     }
-  },
-};
+  }
+}
